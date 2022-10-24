e14075c317d91424282aba59f4cc6a04eba3dafc
sindre <sindre.e@live.com>
2022-10-24 15:15:04 +0200

    adds basic static data

---
+++ gen_static_data.py
@@ -0,0 +1,37 @@
+
+
+from cmath import sin
+from math import cos, floor, pi, sin
+
+def vec_from_angle(angle_rad):
+    return [cos(angle_rad), sin(angle_rad)]
+
+def convert_to_fixed_point(number, int_bit_size=18, fractional_bit_size=14):
+    return "{}'h".format(int_bit_size+fractional_bit_size) + hex(floor(number*2**fractional_bit_size))[2:]
+
+
+
+board = [
+    "11111111",
+    "10101011",
+    "10000001",
+    "11010101",
+    "10000001",
+    "10001011",
+    "10000001",
+    "11111111"
+         ]
+
+player_pos_init = [2.5, 5.5]
+player_direction = vec_from_angle(pi/4)
+
+# for i in range(len(player_direction)):
+#     print("player_direction[{}] = {};".format(i, convert_to_fixed_point(player_direction[i])))
+
+for i in range(len(player_pos_init)):
+    print("player_pos[{}] = {};".format(i, convert_to_fixed_point(player_pos_init[i])))
+
+
+
+# for i in range(len(board)):
+#     print("bit_map[{}] = 8'b{};".format(i,board[i]))
