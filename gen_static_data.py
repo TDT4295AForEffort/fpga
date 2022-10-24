0e47424b06d03f255e5c7c1f712f7275e7aacc52
sindre <sindre.e@live.com>
2022-10-24 18:45:34 +0200

    adds half baked beginning to raycasting module

--- gen_static_data.py
+++ gen_static_data.py
@@ -7,7 +7,12 @@ def vec_from_angle(angle_rad):
     return [cos(angle_rad), sin(angle_rad)]
 
 def convert_to_fixed_point(number, int_bit_size=18, fractional_bit_size=14):
-    return "{}'h".format(int_bit_size+fractional_bit_size) + hex(floor(number*2**fractional_bit_size))[2:]
+    num = hex(floor(number*2**fractional_bit_size))
+    sign = ''
+    if(num[0] == '-'): 
+        sign = '-'
+        num = num[1:]
+    return "{}'h".format(int_bit_size+fractional_bit_size) + sign + num[2:]
 
 
 
@@ -24,14 +29,15 @@ board = [
 
 player_pos_init = [2.5, 5.5]
 player_direction = vec_from_angle(pi/4)
+hfov = 90
 
 # for i in range(len(player_direction)):
 #     print("player_direction[{}] = {};".format(i, convert_to_fixed_point(player_direction[i])))
 
-for i in range(len(player_pos_init)):
-    print("player_pos[{}] = {};".format(i, convert_to_fixed_point(player_pos_init[i])))
-
-
+# for i in range(len(player_pos_init)):
+#     print("player_pos[{}] = {};".format(i, convert_to_fixed_point(player_pos_init[i])))
 
 # for i in range(len(board)):
 #     print("bit_map[{}] = 8'b{};".format(i,board[i]))
+
+print(convert_to_fixed_point(0.99))
