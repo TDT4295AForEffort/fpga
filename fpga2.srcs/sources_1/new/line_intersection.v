1d902e954e166a9a538b60da35d597f1ebe770ef
sindre <sindre.e@live.com>
2022-10-31 14:47:24 +0100

    adds better line_intersection module. (Untested)

--- fpga2.srcs/sources_1/new/line_intersection.v
+++ fpga2.srcs/sources_1/new/line_intersection.v
@@ -21,84 +21,61 @@
 
 
 module line_intersection(
-    input wire signed [31:0] o_a[1:0],
-    input wire signed [31:0] o_b[1:0],
-    input wire signed [31:0] o_c[1:0],
-    input wire signed [31:0] o_d[1:0],
+    
+    input clk100,
+    input wire signed [31:0] x1,
+    input wire signed [31:0] y1,
+    input wire signed [31:0] x2,
+    input wire signed [31:0] y2,
+    input wire signed [31:0] x3,
+    input wire signed [31:0] y3,
+    input wire signed [31:0] x4,
+    input wire signed [31:0] y4,
     input wire start,
     output wire collision,
-    output wire signed [31:0] col_point[1:0],
+    output wire signed [31:0] col_point_x,
+    output wire signed [31:0] col_point_y,
     output wire read_ready
     );
 
-    wire signed [31:0] v_ab[1:0];
-    assign v_ab[0] = o_b[0] - o_a[0];
-    assign v_ab[1] = o_b[1] - o_a[1];
-
-    wire signed [31:0] v_ac[1:0];
-    assign v_ac[0] = o_c[0] - o_a[0];
-    assign v_ac[1] = o_c[1] - o_a[1];
-
-    wire signed [31:0] v_ad[1:0];
-    assign v_ad[0] = o_d[0] - o_a[0];
-    assign v_ad[1] = o_d[1] - o_a[1];
-
-    wire signed [31:0] v_cd[1:0];
-    assign v_cd[1] = o_d[1] - o_c[1];
-    assign v_cd[0] = o_d[0] - o_c[0];
-
-    wire signed [31:0] v_ca[1:0];
-    assign v_ca[0] = o_a[0] - o_c[0];
-    assign v_ca[1] = o_a[1] - o_c[1];
-
-    // Output
-    reg signed [31:0] scale_factor = 0;
-    reg signed [31:0] x_out[1:0];
+    //Output
     reg collision_reg = 0;
-    reg read_ready_reg = 0;
-    assign col_point[0] = x_out[0] = 0;
-    assign col_point[1] = x_out[1] = 0;
     assign collision = collision_reg;
-    assign read_ray_ready = read_ray_ready_reg;
-
-    reg signed [31:0] ab_dot_ac;
-    reg signed [31:0] ab_dot_ad;
-    reg signed [31:0] ac_dot_ad;
+    reg read_ready_reg = 0;
+    assign read_ready = read_ready_reg;
+    reg col_point_x_reg = 0;
+    assign col_point_x = col_point_x_reg;
+    reg col_point_y_reg = 0;
+    assign col_point_y = col_point_y_reg;
 
     reg [9:0] state = 0;
+    reg signed [31:0] s = 0;
+    reg signed [31:0] t = 0;
+
     always @(posedge clk100) begin
         if(state == 0 && start == 1) begin
-            scale_factor <= (v_ab[0]*v_ac[0] + v_ab[1]*v_ac[1])/(v_ab[0]*v_ab[0] + v_ab[0]*v_ab[0]);
-            ab_dot_ac = v_ab[0]*v_ac[0] + v_ab[1]*v_ac[1];
-            ab_dot_ad = v_ab[0]*v_ad[0] + v_ab[1]*v_ad[1];
-            ac_dot_ad = v_ac[0]*v_ad[0] + v_ac[1]*v_ad[1];
+            read_ready_reg <= 0;
+            s <= ((y3-y1)*(x4-x3) - (x3-x1)*(y4-y3))/((x4-x3)*(y2-y1) - (x2-x1)*(y4-y3)) << 14;
+            t <= ((x2-x1)*(y3-y1)-(y2-y1)*(x3-x1))/((x4-x3)*(y2-y1)-(x2-x1)*(y4-y3)) << 14;
             state <= 1;
         end
 
-        if(state >= 1 && state =< 4) begin // Wait for div
+        if(state >= 1 && state <= 5) begin 
             state <= state + 1;
         end
 
-        if(state == 5) begin 
-            x_out[0] <= scale_factor*v_ab[0];
-            x_out[1] <= scale_factor*v_ab[1];
-            state <= 6;
+        if(state == 6) begin 
+            if(0 <= s && t <= 1) begin
+                collision_reg <= 1;
+                col_point_x_reg <= x1 + (s*(x2-x1) >> 14);
+                col_point_y_reg <= y1 + (s*(y2-y1) >> 14);
+            end else begin 
+                collision_reg <= 0;
+                col_point_x_reg <= 0;
+                col_point_y_reg <= 0;
+            end
+            read_ready_reg <= 1;
+            state <= 0;
         end
-        
-        // This math is gonna break
-        // Check out this: https://en.wikipedia.org/wiki/Intersection_(geometry)#Two_line_segments
-        // and this: https://en.wikipedia.org/wiki/Cramer%27s_rule
-        // Try it in python
-        if(state == 6) begin                                                                // Condition for intersection
-            if( ab_dot_ac > 0 &&                                                            // 0 < v_ab * v_ac < v_ac * v_ad
-                ab_dot_ac < ac_dot_ad &&                                                    //
-                ab_dot_ad > 0 &&                                                            // 0 < v_ab * v_ad < v_ac * v_ad
-                ab_dot_ad < ac_dot_ad &&                                                    //
-                v_ab[0]*v_ab[0] + v_ab[1]*v_ab[1] >= x_out[0]*x_out[0] + x_out[1]*x_out[1]) // |v_ab| >= |x_out|
-                begin 
-                    
-                end
-        end
-        
     end
 endmodule
