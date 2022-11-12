785ddf85f36a01bd02792bdc58264b95eda7f8cb
Ole Peder Brandtzæg <olepbr@gmail.com>
2022-11-12 01:40:50 +0100

    try to fix byte_ready by checking on negedge

--- fpga2.srcs/sources_1/new/spi_slave.v
+++ fpga2.srcs/sources_1/new/spi_slave.v
@@ -31,8 +31,8 @@ always @(posedge clk)
     ssr <= {ssr[1:0], ss};
 
 wire ss_active = ~ssr[1];                  // SS is active low
-wire ss_startmessage = (ssr[2:1]==2'b10);  // message starts at falling edge
-wire ss_endmessage = (ssr[2:1]==2'b01);    // message stops at rising edge
+wire ss_falling = (ssr[2:1]==2'b10);  // message starts at falling edge
+wire ss_rising = (ssr[2:1]==2'b01);    // message stops at rising edge
 
 // and for mosi
 reg [1:0] mosir = 0;
@@ -45,23 +45,24 @@ wire mosi_data = mosir[1];
 reg [2:0] bitcnt = 0;
 
 reg byte_received = 0;              // set to high when we've got a byte
+reg byte_received_fall = 0;
+reg byte_received_delay = 0;
 reg [7:0] byte_data_received = 0;
-reg byte_received_last_cycle = 0;
-assign byte_ready = (byte_received & !byte_received_last_cycle);
+always @(negedge clk) byte_received_fall <= byte_received;
+always @(negedge clk) byte_received_delay <= byte_received_fall;
+assign byte_ready = (byte_received_fall && ~byte_received_delay);
 
 always @(posedge clk)
 begin
-  if(~ss_active)
-    bitcnt <= 3'b000;
+  if(~ss_active) bitcnt <= 3'd0;
   else
   if(sclk_risingedge)
   begin
-    bitcnt <= bitcnt + 3'b001;
-
+    bitcnt <= bitcnt + 3'd1; // increase count
     // MSB comes first, so we use a shift-left register
-    byte_data_received[7:1] = byte_data_received[6:0];
-    byte_data_received[0] = mosi_data;
+    byte_data_received <= {byte_data_received[6:0], mosi_data};
   end
+  if (ss_falling) bitcnt <= 3'd0; // transmission is starting, reset the count
 end
 
 // for every clock cycle, check that:
@@ -69,7 +70,7 @@ end
 // 2. we're on the rising edge of SCLK
 // 3. the bit count is 7 (i.e. the bit we're now receiving is the last of
 // the byte)
-always @(posedge clk) byte_received <= ss_active && sclk_risingedge && (bitcnt==3'b111);
+always @(posedge clk) byte_received <= ss_active && sclk_risingedge && (bitcnt==3'd7);
 
 // stick the byte into the out register
 always @(posedge clk) if(byte_received) outr <= byte_data_received;
@@ -77,12 +78,12 @@ always @(posedge clk) if(byte_received) outr <= byte_data_received;
 reg [7:0] byte_data_sent = 0;
 
 reg [7:0] cnt = 0;
-always @(posedge clk) if(ss_startmessage) cnt<=cnt+8'h1;  // count the messages
+always @(posedge clk) if(ss_falling) cnt<=cnt+8'h1;  // count the messages
 
 always @(posedge clk)
 if(ss_active)
 begin
-  if(ss_startmessage)
+  if(ss_falling)
     byte_data_sent <= cnt;  // first byte sent in a message is the message count
   else
   if(sclk_fallingedge)
@@ -90,8 +91,7 @@ begin
     if(bitcnt==3'b000)
       byte_data_sent <= 8'h00;  // after that, we send 0s
     else
-      byte_data_sent[7:1] = byte_data_sent[6:0];
-      byte_data_sent[0] = 1'b0;
+      byte_data_sent <= {byte_data_sent[6:0],0};
   end
 end
 
